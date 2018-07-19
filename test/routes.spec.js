9a2eb484d5134910c14e1116d5cfdacea25cfaa2
Jack Laird <jack.laird0@gmail.com>
2018-07-18 19:49:15 -0600

    Write test for get games endpoint

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,8 +1,8 @@
-const environment = process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../server');
 const should = chai.should();
+const server = require('../server');
+const environment = process.env.NODE_ENV = 'test';
 const configuration = require('../knexfile')[environment];
 const knex = require('knex')(configuration);
 
@@ -16,5 +16,38 @@ describe('Client Routes', () => {
         resp.should.have.status(404);
         done();
       });
+  });
+});
+
+describe('API Routes', () => {
+  
+  beforeEach( done => {
+    knex.migrate.rollback()
+      .then(() => {
+        knex.migrate.latest()
+          .then(() => {
+            return knex.seed.run()
+              .then(() => {
+                done();
+              });
+          });
+      });
+  });
+
+  describe('GET /api/v1/games', () => {
+    it('should return an array of game objects', done => {
+      chai.request(server)
+        .get('/api/v1/games')
+        .end((err, resp) => {
+          resp.should.have.status(200);
+          resp.should.be.json;
+          resp.body.should.be.a('array');
+          resp.body[0].should.have.property('id')
+          resp.body[0].id.should.equal(1)
+          resp.body[0].should.have.property('level_name')
+          resp.body[0].level_name.should.equal('CSS-01')
+          done();
+        });
+    });
   });
 });
